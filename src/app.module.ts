78698c2f82f95717cc5e43045ebca3cb5a70e80b
rooberttdev <robertbarbosa.dev@gmail.com>
2024-10-03 08:18:05 -0400

    Ajeitando o module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,19 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    SequelizeModule.forRootAsync({
-      useFactory: (configService: ConfigService) => ({
-        dialect: 'mysql',
-        host: configService.get<string>('DATABASE_HOST'),
-        port: +configService.get<number>('DATABASE_PORT'),
-        username: configService.get<string>('DATABASE_USER'),
-        password: configService.get<string>('DATABASE_PASSWORD'),
-        database: configService.get<string>('DATABASE_NAME'),
-        autoLoadModels: true,
-        synchronize: true,
-      }),
-      inject: [ConfigService],
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: '35.198.56.143',
+      port: 3306,
+      username: 'admin-wenlock',
+      password: 'root',
+      database: 'wenlock_backend',
+      autoLoadModels: true,
+      synchronize: true,
     }),
     UsersModule,
     AuthModule,
